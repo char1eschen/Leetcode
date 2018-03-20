740e8fd285ca2e0719f7c5ac07412462e4c9ece8
char1eschen <martian2005@gmail.com>
2018-03-20 21:54:11 +1100

    Update Two Sum

--- TwoSum.js
+++ TwoSum.js
@@ -23,10 +23,9 @@
  * @param {number} target
  * @return {number[]}
  */
-var i, j;
 var twoSum = function (nums, target) {
-    for(i = 0; i < nums.length - 1; i++) {
-        for(j = i + 1; j < nums.length; j++) {
+    for(var i = 0; i < nums.length - 1; i++) {
+        for(var j = i + 1; j < nums.length; j++) {
             if(nums[i] + nums[j] == target) {
                 return [i, j];
             }
